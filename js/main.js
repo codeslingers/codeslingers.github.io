be342dde4750e50c5234eaf21f32439422e5df18
dcassiani <dcassiani@gmail.com>
2019-07-07 05:44:03 -0300

    api call

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $( "#leia-presenca , #recolha-presenca" ).each( function(){
-      $(this).click(function() {
-        toggleLeitura("presenca");
-      });
-    });
-
-
+    buildSaibaMais("influencia");
+    buildSaibaMais("presenca");
+    buildSaibaMais("sistemas");
 
 });
+
+function buildSaibaMais(baseId){
+  $( "#leia-"+baseId+" , #recolha-"+baseId ).each( function(){
+    $(this).click(function() {
+      toggleLeitura(baseId);
+    });
+  });
+}
 
 function toggleLeitura(baseId){
     $("#leia-"+baseId ).toggle( "slow");
     $("#texto-"+baseId ).toggle( "slow");
     $("#recolha-"+baseId ).toggle( "slow");
+    $.get( "http://bibino1.jelastic.saveincloud.net/go/to/CSGHome-"+baseId);
 }
